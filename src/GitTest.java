45394d527e1a7b3c67665f11325701b7dd791028
dungtm <dungtm@wisekey.ch>
2010-07-30 17:45:54 +0700

    V2

--- src/GitTest.java
+++ src/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT 2");
+		System.out.println("Hello GIT 3");
 	}
 
 }
