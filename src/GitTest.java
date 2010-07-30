d6b04197d58645f703ce37c9beb18c98745533a3
tuanlv <tuanlv1983@gmail.com>
2010-07-30 17:20:45 +0700

    v2

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello GIT");
+		System.out.println("Hello GIT 2");
 	}
 
 }
